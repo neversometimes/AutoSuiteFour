6d663f77c92f796c83e9498b335fe0e82d4e8f4c
Brian <neversometimes@icloud.com>
2024-09-29 18:57:51 -0700

    added getBrowserName(). added headless options. removed superfluous code.

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,12 +1,16 @@
 package base;
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.devtools.DevTools;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -17,26 +21,46 @@ import java.time.Duration;
 public class BaseTest {
 
     public WebDriver driver;
+    public DevTools devTools;
     SoftAssert sa = new SoftAssert();
+    public String initPage = "https://bonigarcia.dev/selenium-webdriver-java/";
+
 
     @BeforeMethod
     public WebDriver setUp() {
 
         // get target browser from CLI if given, otherwise use "chrome"
-        String browserName = System.getProperty("browser") != null ? System.getProperty("browser") : "chrome";
+        String browserName = System.getProperty("browser") != null ? System.getProperty("browser") : "edge";
 
         if (browserName.contains("chrome")) {
             ChromeOptions options = new ChromeOptions();
-            if (browserName.contains("headless")){  // if headless param given on CLI
-                options.addArguments("headless");       // then run test in headless mode
+            if (browserName.contains("headless")){      // if headless param given on CLI
+                options.addArguments("--headless");       // then run test in headless mode
             }
-            WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver(options);
-        } else if (browserName.equalsIgnoreCase("firefox")) {
-            driver = new FirefoxDriver();
+            options.addArguments("--use-fake-ui-for-media-stream");     // used for getUserMediaTest
+            options.addArguments("--use-fake-device-for-media-stream");
 
-        } else if (browserName.equalsIgnoreCase("edge")) {
-            driver = new EdgeDriver();
+            driver = new ChromeDriver(options);
+
+            devTools = ((ChromeDriver) driver).getDevTools();
+            devTools.createSession();
+        } else if (browserName.contains("firefox")) {
+            FirefoxOptions options = new FirefoxOptions();
+            if (browserName.contains("headless")){      // if headless param given on CLI
+                options.addArguments ("-headless");      // then run test in headless mode
+            }
+
+            driver = new FirefoxDriver(options);
+
+        } else if (browserName.contains("edge")) {
+            EdgeOptions options = new EdgeOptions();
+            if (browserName.contains("headless")) {     // if headless param given on CLI
+                options.addArguments("--headless");       // then run test in headless mode
+            }
+            options.addArguments("--use-fake-ui-for-media-stream");     // used for GetUserMediaTest
+            options.addArguments("--use-fake-device-for-media-stream");
+
+            driver = new EdgeDriver(options);
         }
         driver.manage().window().setSize(new Dimension(1440, 900));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));  // basic 5s implicit wait
@@ -46,12 +70,18 @@ public class BaseTest {
 
     @AfterMethod
     public void tearDown() {
+
         driver.quit();
     }
 
 
     public void goToURL(String url) {
         driver.get(url);
+    }
+
+    public String getBrowserName () {
+         Capabilities cap = ((RemoteWebDriver) driver).getCapabilities();
+         return cap.getBrowserName();
     }
 
 }
